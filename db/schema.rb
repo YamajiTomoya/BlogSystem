dbb1de9c8771c994650d2cdcb8d94ab7a6623606
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-04-22 16:23:51 +0900

    Articlesモデル・コントローラー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_070557) do
+ActiveRecord::Schema.define(version: 2019_04_22_071927) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.boolean "open_flg", default: false
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
