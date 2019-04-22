e98126f0ec28538a686b58bc8480bd3e676db1c3
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-04-22 16:32:21 +0900

    Articleモデルにバリデーションを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
     belongs_to :user
+    validates :title, {presence: true}
+    validates :content, {presence: true}
+    validates :open_flg, {presence: true}
+    validates :author, {presence: true}
 end
