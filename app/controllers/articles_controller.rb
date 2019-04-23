7c31d98e7d29aaef5715b6b0304c6b94b6cca7fc
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-04-23 13:52:15 +0900

    新規記事作成時に、ユーザー記事一覧ページに遷移するように変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,9 +12,8 @@ class ArticlesController < ApplicationController
     def create
         article = Article.new(title: params[:title], content: params[:content], open_flg: params[:open_flg], user_id: @current_user.id)
         if article.save
-            puts "saved!"
+            redirect_to("/users/#{@current_user.username}")
         end
-        puts article.errors.full_messages
     end
 
     def edit
