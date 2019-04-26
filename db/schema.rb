631319c99c3b214b5b6e706b18e91ab79db25515
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-04-26 15:36:45 +0900

    schemaが変更

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2019_04_26_052738) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
-    t.integer "user_id"
     t.integer "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
