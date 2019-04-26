0e7a9c53b2a24dc0ebf13b2bc8ffd49443a35edd
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-04-26 17:11:03 +0900

    indexページから、ログイン状態かどうかによってリダイレクトを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
     def index
         if current_user
             redirect_to(user_page_path(current_user.username))
+        else
+            redirect_to(new_user_session_path)
         end
     end
     def signup
