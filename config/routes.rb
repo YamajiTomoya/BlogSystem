bcf3d5daf17053e0560d14aff98e64a33eab6ac4
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-04-26 14:36:36 +0900

    devise用のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  # devise_for :users
   # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # devise_for :users, controllers: { passwords: 'passwords' }
-  # root "users#login_form", as: :login_form
-  # get "signup" => "users#signup"
-  # post "login" => "users#login"
-  # post "logout" => "users#logout"
-  # get "users/:username" => "users#show"
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
   # get "users/:username/articles" => "articles#index", as: :user_page
-  # post "users" => "users#create"
-
   resources :articles do
     # comment[s]じゃないとform_withが動きません
     post "comments" => "comments#create"
