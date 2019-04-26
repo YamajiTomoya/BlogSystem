a9f157b8f1f414c56b0495383b2f6fa13c7b531f
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-04-26 14:30:05 +0900

    usernameが保存できるように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,21 @@
 class ApplicationController < ActionController::Base
-    before_action :set_current_user
-    private
+  before_action :set_current_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def set_current_user
-        @current_user = User.find_by(id: session[:user_id])
+  private
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def forbid_login_user
+    if @current_user
+      redirect_to(user_page_path(@current_user.username), notice: "すでにログインしています")
     end
-    
-    def forbid_login_user
-      if @current_user
-        redirect_to(user_page_path(@current_user.username), notice: "すでにログインしています")
-      end
-    end
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
