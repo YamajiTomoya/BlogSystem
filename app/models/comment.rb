76be5c3d8b0e475bed03762663a65686f5a98b7c
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-05-07 17:25:32 +0900

    Commentモデルにバリデーションを追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ApplicationRecord
-    #TODO: ユーザーと記事の両方が削除されなければ削除されていない（どちらか片方が消えれば消えるようにしたい）
     belongs_to :user
     belongs_to :article
+
+    validates :content, {presence: true}
 
     def user
         return User.find(self.user_id)
