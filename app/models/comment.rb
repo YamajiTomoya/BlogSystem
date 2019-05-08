0ed1985c5250db0c1ed407fbc1cbfcffef3ff53e
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-05-08 15:38:24 +0900

    非ログインユーザー用に記事詳細ページを修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,11 +10,17 @@ class Comment < ApplicationRecord
 
     def deletable?(current_user)
         # あるコメントに対して、削除可能かどうか判定します
+        unless current_user
+            return false
+        end
         return current_user.id == self.user_id || current_user.id == self.article.user_id
     end
 
     def poster?(current_user)
         # あるコメントに対して、編集可能かどうか(投稿者かどうか)を判定します
+        unless current_user
+            return false
+        end
         return current_user.id == self.user_id
     end
 end
