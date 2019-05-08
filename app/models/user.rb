d72511e26b56ff087cceb247d856d214e69ee57e
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-05-08 10:13:38 +0900

    emailにユニーク属性を付与

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :username, {presence: true, uniqueness: true}
-  validates :email, presence: true
+  validates :email, {presence: true, uniqueness: true}
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable
   
