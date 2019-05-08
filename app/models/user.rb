c7b0483c50cd134894e0df23797a2eaa3639b48e
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-05-08 11:12:53 +0900

    deviseのエラーメッセージを編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :username, {presence: true, uniqueness: true}
   validates :email, {presence: true, uniqueness: true}
+  validates :password, presence: true
+
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable
   
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
+
   # deviseでemailを不必要にする
   def email_required?
     false
   end
+  
   def email_changed?
     false
   end
