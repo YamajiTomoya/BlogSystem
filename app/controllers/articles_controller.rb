96201cfbaf8f88deaa00dc5ea300e50f0a17fe37
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-05-09 11:21:41 +0900

    記事一覧ページにページネーション機能を追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
 
     def index
         @user = User.find_by(username: params[:username])
-        @articles = Article.where(user_id: @user.id)
+        @articles = Article.page(params[:page]).per(3).where(user_id: @user.id)
         @author_flg = false
         # 執筆者は全ての記事を見れますが、非ログインユーザーはカレントユーザーのidを持っていないので、このような記述になっています
         unless current_user
