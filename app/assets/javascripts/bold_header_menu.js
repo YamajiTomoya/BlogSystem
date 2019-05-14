a06ce1bc13ec5a85d02fd0f901105f5675c0e1e3
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-05-14 17:19:50 +0900

    ヘッダーメニューhover時に太字に

---
+++ app/assets/javascripts/bold_header_menu.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('.header-menus li').hover(function () {
+        $(this).css('font-weight', 'bold');
+    }, function () {
+        $(this).css('font-weight', 'normal');
+    });
+})
