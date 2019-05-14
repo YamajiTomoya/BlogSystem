e2f487d913adf4c4d70eb58d15e70ec2ff5a6cb5
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-05-14 17:48:51 +0900

    ヘッダーメニューホバー時のエフェクトを変更

--- app/assets/javascripts/header_menu_effect.js
+++ app/assets/javascripts/header_menu_effect.js
@@ -1,7 +1,14 @@
 $(function () {
     $('.header-menus li').hover(function () {
         $(this).css('font-weight', 'bold');
+        $(this).css({
+            transform: 'rotate(24deg)'
+        });
     }, function () {
         $(this).css('font-weight', 'normal');
+        $(this).css({
+            transform: 'rotate(0)'
+        });
+
     });
 })
