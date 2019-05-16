8e33703745fdb9cfc78033231ca73f9cf57a7bcd
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-05-16 13:17:19 +0900

    #19 i18nにおいてlocaleの取得・設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -7,5 +8,15 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username email])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  def set_locale
+    I18n.locale = extract_locale_from_tld || I18n.default_locale
+  end
+
+  # トップレベルドメインからロケールを取得する、なければ+nil+を返す
+  def extract_locale_from_tld
+    parsed_locale = request.host.split('.').last
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
   end
 end
