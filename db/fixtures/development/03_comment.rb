ba871e5c54fa35679d1956e535cc6d654887c591
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-05-16 16:07:10 +0900

    修正漏れ

--- db/fixtures/development/03_comment.rb
+++ db/fixtures/development/03_comment.rb
@@ -2,7 +2,7 @@ COMMENT_NUM = 50
 (1..COMMENT_NUM).each do |i|
   Comment.seed do |s|
     s.id = i
-    s.content = Faker::Lorem.sentence(random_words_to_add=10)
+    s.content = Faker::Lorem.sentence(random_words_to_add = 10)
     s.user = User.find(rand(USER_NUM) + 1)
     s.article = Article.find(rand(ARTICLE_NUM) + 1)
   end
