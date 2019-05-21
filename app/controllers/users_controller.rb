fad3fb178ef4b0fca4273dc6c6f1d20b2ede3169
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-05-21 11:03:54 +0900

    リファクタリング：ルーティングの整形、伴い不要になったファイルの削除

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationController
-  def index
-    if current_user
-      redirect_to(user_page_path(current_user.username))
-    else
-      redirect_to(new_user_session_path)
-    end
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
   # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
-  devise_for :users
+  root 'home#allocate'
+  devise_for :users, only: [:sessions, :password]
+  as :user do
+    get 'users/sign_up', to: 'devise/registrations#new', as: :new_user_registration
+    post 'users', to: 'devise/registrations#create', as: :user_registration
+  end
 
-  get 'users/:username/statistics' => 'user_statistics#index', as: :user_statistics
-  post 'users/:username/statistics' => 'user_statistics#create', as: :user_statistic
-  get 'users/:username/statistics/dounload/:id' => 'user_statistics#download', as: :user_statistic_download
-  get 'users/:username/articles'=> 'articles#index', as: :user_page
+  get 'users/:username/articles', to: 'articles#index', as: :user_page
+
+  get 'users/:username/statistics', to: 'user_statistics#index', as: :user_statistics
+  post 'users/:username/statistics', to: 'user_statistics#create', as: :user_statistic
+  get 'users/:username/statistics/dounload/:id', to: 'user_statistics#download', as: :user_statistic_download
+
 
   resources :articles do
-    resources :comments, :only =>[
+    resources :comments, only: [
       :create, :edit, :update, :destroy
     ]
   end
