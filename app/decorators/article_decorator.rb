83d5e180f6f92a6cf1792cafd80026981388a6c4
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-05-23 13:09:47 +0900

    #55 記事一覧画面における予約投稿されている記事はそのことがわかるように

--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -9,6 +9,10 @@ class ArticleDecorator < Draper::Decorator
     object.updated_at.strftime('%Y-%m-%d %H:%M')
   end
 
+  def post_reservation_at
+    object.post_reservation_at.strftime('%Y-%m-%d %H:%M') unless object.post_reservation_at.nil?
+  end
+
   # 一覧ページに収まり切らないような長い文章は、途中で区切る
   def content
     object.content.size <= 150 ? object.content : object.content.slice(0, 150) + '...'
