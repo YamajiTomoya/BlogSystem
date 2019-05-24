a9339c2711a11912b14c2221365f024bb54539fc
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-05-24 09:33:14 +0900

    #47 インクリメンタルサーチの実装(ちゃんとajaxを使って)

--- app/assets/javascripts/incremental_search.js
+++ app/assets/javascripts/incremental_search.js
@@ -1,16 +1,40 @@
 $(function () {
-    $search_form = $('.search-form');
-    $search_button = $('#search-submit');
+    $forms = $('.search-form').find('input');
     $clear_button = $('#clear-button');
 
-    // フォームへの入力を検知して、検索ボタンをクリック
-    $search_form.on('keyup change', function () {
-        $search_button.click();
+    // ransackにまとめて検索項目を渡す
+    search_params = {
+        'q': {}
+    }
+
+    // ransackに渡すパラメータの更新
+    function renew_search_params() {
+        for (let index = 0; index < $forms.length; index++) {
+            const element = $forms.eq(index);
+            const key = element.attr('id').slice(2); // q_以降を抜き出す
+            const value = element.val();
+            search_params['q'][key] = value;
+        }
+    }
+
+    ajaxPost = function (search_params) {
+        $.ajax({
+            url: location.href,
+            type: 'GET',
+            data: search_params
+        });
+    };
+
+    // フォームへの入力を検知して、パラメータを更新。ajaxに渡す
+    $forms.on('keyup change', function () {
+        renew_search_params();
+        ajaxPost(search_params);
     })
 
-    // フォームを一度空にして、検索ボタンをクリック
+    // フォームを空にして、パラメータ更新。ajaxに渡す
     $clear_button.on('click', function () {
-        $(".search-form").val("");
-        $search_button.click();
+        $forms.val('');
+        renew_search_params();
+        ajaxPost(search_params);
     });
 });
