f3dd2dbe7b25989189cdc2aeef3240b01da29261
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-05-28 10:42:35 +0900

    #49 非同期処理のリファクタリング

--- app/jobs/user_statistics_job.rb
+++ app/jobs/user_statistics_job.rb
@@ -5,20 +5,17 @@ class UserStatisticsJob < ApplicationJob
   # ユーザー統計情報を収集し、DB、csvファイルに保存
   def perform(us_id, user, aggregates)
     us = UserStatistic.find(us_id)
-
-    # ActiveJobにはシンボルを渡せないので、ここで変換
-    aggregates = aggregates.map(&:to_sym)
     us_data = UserStatisticsData.new(user, aggregates)
 
     # 時間がかかる処理を再現するため10秒眠らせる
     sleep(10)
 
-    # 統計情報を生成、用いて更新
+    # 統計情報を生成
     us_data.generate_data
-    us.update(us_data.data)
 
-    # csv_dataを更新、保存時にcsvファイル作成が走る
+    # データベースを更新、保存時にcsvファイル作成が走る
+    us.status = :completed
     us.csv_data = us_data.to_csv
-    us.update(status: :completed)
+    us.update(us_data.data)
   end
 end
--- app/models/user_statistics_data.rb
+++ app/models/user_statistics_data.rb
@@ -9,24 +9,20 @@ class UserStatisticsData
     # 集計開始時刻は必ず含める
     @aggregates.push(:start_aggregating_at)
     @data = {}
-    @convert_to_header = {
-      article_count: '記事数',
-      comment_count: 'コメント数',
-      start_aggregating_at: '集計開始時刻'
-    }
   end
 
   # 統計情報を生成
   def generate_data
     @aggregates.each do |aggregate|
       value = send("calc_#{aggregate}")
-      @data[aggregate.to_sym] = value
+      @data[aggregate] = value
     end
   end
 
   # csvフォーマットされたデータを生成
   def to_csv
-    headers = @aggregates.map { |aggregate| @convert_to_header[aggregate] }
+    prefix = 'activerecord.attributes.user_statistics.'
+    headers = @aggregates.map { |aggregate| I18n.t(prefix + aggregate.to_s) }
     csv_data = CSV.generate do |csv|
       csv << headers
       csv << @data.values
@@ -45,6 +41,6 @@ class UserStatisticsData
 
   def calc_start_aggregating_at
     now = Time.zone.now
-    return now.strftime('%Y-%m-%d %H:%M:%S')
+    now.strftime('%Y-%m-%d %H:%M:%S')
   end
 end
