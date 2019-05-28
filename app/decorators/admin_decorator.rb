5e6ec18f1045eddd63224d0817916184ee33c719
Tomoya Yamaji <yamaji@fusic.co.jp>
2019-05-28 11:02:56 +0900

    #46 decorationの紛らわしい名前を修正

--- app/decorators/admin_decorator.rb
+++ app/decorators/admin_decorator.rb
@@ -1,7 +1,7 @@
 class AdminDecorator < Draper::Decorator
   delegate_all
 
-  def created_at
+  def created_at_formatted
     object.created_at.strftime('%Y-%m-%d %H:%M')
   end
 end
--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -1,20 +1,20 @@
 class ArticleDecorator < Draper::Decorator
   delegate_all
 
-  def created_at
+  def created_at_formatted
     object.created_at.strftime('%Y-%m-%d %H:%M')
   end
 
-  def updated_at
+  def updated_at_formatted
     object.updated_at.strftime('%Y-%m-%d %H:%M')
   end
 
-  def post_reservation_at
+  def post_reservation_at_formatted
     object.post_reservation_at.strftime('%Y-%m-%d %H:%M')
   end
 
   # 一覧ページに収まり切らないような長い文章は、途中で区切る
-  def content
+  def shrink_content
     object.content.size <= 150 ? object.content : object.content.slice(0, 150) + '...'
   end
 end
